6bddbe28b2d854e63030efa961a316c4b88d4468
anil843324 <anilkrsingh14200@gmail.com>
2022-06-28 16:36:16 +0530

    question explain added

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,9 @@
 
 // expected output   1 2 Fizz 4 Buzz 6 7 8 Fizz Buzz
+
+/*
+All natural numbers divisible by 3 and not by 5 are called as "Fizz" while those divisible by 5 and not by 3 are called as "Buzz". If any number is divisible by both it's called as "FizzBuzz". Rest all natural numbers remain in their original form.
+*/
 const FizzBuzz = (n) => {
 
     let p = ''
