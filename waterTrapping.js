4f713ec345a7c25d5447ea4c986075eca9d2afe0
anil843324 <anilkrsingh14200@gmail.com>
2022-08-03 09:33:28 +0530

    solved

---
+++ waterTrapping.js
@@ -0,0 +1,47 @@
+const trapping = (arr, n) => {
+  let lmax = [];
+  let rmax = [];
+
+  for (let i = 0; i < n; i++) {
+    lmax.push(0);
+    rmax.push(0);
+  }
+
+  lmax[0] = arr[0];
+
+ rmax[n-1]=arr[n-1]
+
+//   left side checking 
+        for(let i=1;i<n;i++){
+
+            let temp=Math.max(arr[i],lmax[i-1])
+             lmax[i]=temp;
+        }
+ 
+// right side
+
+ 
+for(let i=n-2;i>=0;i--){
+
+    let temp=Math.max(arr[i],rmax[i+1])
+     rmax[i]=temp;
+}
+
+let water=0;
+
+  for(let  i=1;i<n-1;i++){
+
+        water=water+Math.min(lmax[i],rmax[i])-arr[i]
+      
+    
+  }
+
+   console.log(water)
+
+
+
+};
+let arr = [3, 0, 0, 2, 0, 4];
+let n = arr.length;
+
+trapping(arr, n);
