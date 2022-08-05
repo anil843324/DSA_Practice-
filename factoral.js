c6d4ecbafd14b926450266d10491c4ee4b3ef712
anil843324 <anilkrsingh14200@gmail.com>
2022-08-05 11:39:38 +0530

    solved

--- factoral.js
+++ factoral.js
@@ -1,23 +1,27 @@
 function factorial(N) {
   //code here
 
-   let arr=[]
-  for(let i=1;i<N;i++){
+  let ans = [];
+  ans.push(1);
 
-     arr.push(i)
+  for (let i = 2; i <= N; i++) {
+    let carry = 0;
+
+    for (let j = 0; j < ans.length; j++) {
+      let val = ans[j] * i + carry;
+      ans[j] = val % 10;
+      carry = Math.floor(val / 10);
+    }
+    while (carry !== 0) {
+      ans.push(carry % 10);
+      carry = Math.floor(carry / 10);
+    }
   }
-  
 
-  
+  ans.reverse();
 
-
-
-
-
-
-
-
+  return ans;
 }
 
-let res=factorial(5);
-console.log(res)
+let res = factorial(5);
+console.log(res);
