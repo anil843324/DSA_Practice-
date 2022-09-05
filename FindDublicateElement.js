cb80b11a538c4dbf04436e04b092eb7776399ed8
anil843324 <anilkrsingh14200@gmail.com>
2022-09-05 11:04:20 +0530

    solved

---
+++ FindDublicateElement.js
@@ -0,0 +1,28 @@
+function FindDublicate(arr) {
+
+  let ans=0;
+
+   for(let i=0;i<arr.length;i++){
+
+     ans=ans^arr[i]
+   }
+  
+
+   for(let i=1;i<arr.length;i++){
+
+    ans=ans^i;
+  }
+ 
+
+
+
+   return ans;
+
+
+
+}
+
+let arr = [6, 3, 1, 5, 4, 3, 2];
+let n = arr.length;
+
+console.log(FindDublicate(arr, n));
